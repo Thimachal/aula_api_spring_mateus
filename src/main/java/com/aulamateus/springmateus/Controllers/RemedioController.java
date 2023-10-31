e3dc1cf239d1d05f1692146d2512041c4341f6c7
Thimachal <thimachal@gmail.com>
2023-10-30 23:24:48 -0300

    controllers

---
+++ src/main/java/com/aulamateus/springmateus/Controllers/RemedioController.java
@@ -0,0 +1,12 @@
+package com.aulamateus.springmateus.Controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cadremedios")
+public class RemedioController {
+    @PostMapping
+    public void register(@RequestBody String remedio){
+
+    }
+}
