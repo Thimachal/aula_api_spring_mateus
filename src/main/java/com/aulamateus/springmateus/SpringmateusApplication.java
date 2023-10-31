26cb8bcaaac07d3c637d5a1db2cdfd4b70dd2974
Thimachal <thimachal@gmail.com>
2023-10-30 22:52:20 -0300

    first commit

---
+++ src/main/java/com/aulamateus/springmateus/SpringmateusApplication.java
@@ -0,0 +1,13 @@
+package com.aulamateus.springmateus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmateusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmateusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aulamateus/springmateus/SpringmateusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulamateus.springmateus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmateusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
