72da300e95bc597b7adcf3c7df9490efd2b061c0
Thimachal <thimachal@gmail.com>
2023-10-31 21:41:33 -0300

    ENUM

--- src/main/java/com/aulamateus/springmateus/Remedy/DadosCadastroRemedio.java
+++ src/main/java/com/aulamateus/springmateus/Remedy/DadosCadastroRemedio.java
@@ -1,11 +1,7 @@
 package com.aulamateus.springmateus.Remedy;
 
 //classe record não prcisa de geters setters e outras parafernalhas
-public record DadosCadastroRemedio() {
-        String name;
-        Via via;
-        String lot;
-        Integer quantity;
-        String validate;
-        Laboratory laboratory;
+public record DadosCadastroRemedio(String name, Via via, Integer lot, Integer quantity, String validate, Laboratory laboratory)
+{
+
 }
