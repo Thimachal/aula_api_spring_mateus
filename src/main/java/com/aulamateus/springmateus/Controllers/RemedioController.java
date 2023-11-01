36e724ad1394f0e2c43b1a32dffa17c88d1651a3
Thimachal <thimachal@gmail.com>
2023-10-31 21:22:46 -0300

    remedy

--- src/main/java/com/aulamateus/springmateus/Controllers/RemedioController.java
+++ src/main/java/com/aulamateus/springmateus/Controllers/RemedioController.java
@@ -1,12 +1,13 @@
 package com.aulamateus.springmateus.Controllers;
 
+import com.aulamateus.springmateus.Remedy.DadosCadastroRemedio;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cadremedios")
 public class RemedioController {
     @PostMapping
-    public void register(@RequestBody String remedio){
+    public void register(@RequestBody DadosCadastroRemedio dados){
 
     }
 
