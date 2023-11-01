bec78e4b56f90ca48ea486e6ee65c59c8aacf43b
Thimachal <thimachal@gmail.com>
2023-11-01 18:09:11 -0300

    remedy

--- src/main/java/com/aulamateus/springmateus/Remedy/Remedy.java
+++ src/main/java/com/aulamateus/springmateus/Remedy/Remedy.java
@@ -1,4 +1,15 @@
 package com.aulamateus.springmateus.Remedy;
 
 public class Remedy {
+
+    private Long id;
+    private String name;
+    private Via via;
+    private Integer lot;
+    private Integer quantity;
+    private String validate;
+    private Laboratory laboratory;
+
+
+
 }
