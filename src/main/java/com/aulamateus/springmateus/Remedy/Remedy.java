60c079e965f3b1225b1ad8b5565704607729f287
Thimachal <thimachal@gmail.com>
2023-11-01 18:10:05 -0300

    remedy

--- src/main/java/com/aulamateus/springmateus/Remedy/Remedy.java
+++ src/main/java/com/aulamateus/springmateus/Remedy/Remedy.java
@@ -1,6 +1,13 @@
 package com.aulamateus.springmateus.Remedy;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Remedy {
+    @Id
 
     private Long id;
     private String name;
